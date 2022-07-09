59afc66bac9bbd900d7197c37f131151f5866aa0
iustin05 <iusgrig@icloud.com>
2022-07-10 00:17:19 +0200

    User Story B1 R1
    - FIXED missing timestamp field in autogenerated database table

--- Server/src/Database/GeneralDatabaseManager.java
+++ Server/src/Database/GeneralDatabaseManager.java
@@ -12,7 +12,7 @@ public class GeneralDatabaseManager {
         String query = "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, password VARCHAR(100), role VARCHAR(15));" +
                 "CREATE TABLE IF NOT EXISTS items (id SERIAL PRIMARY KEY, name VARCHAR(50), quantity INT, price INT, priceControl BOOLEAN);" +
                 "CREATE TABLE IF NOT EXISTS preferences (preference VARCHAR(20), value varchar(50));" +
-                "CREATE TABLE IF NOT EXISTS checkouts (id INT, itemID INT REFERENCES items(id), quantity INT, price INT);" +
+                "CREATE TABLE IF NOT EXISTS checkouts (id INT, itemID INT REFERENCES items(id), quantity INT, price INT, timestamp VARCHAR(50));" +
                 "INSERT INTO preferences (preference, value) values ('workingHours', '09:00 17:00') ON CONFLICT DO NOTHING;" +
                 "INSERT INTO users (password, role) VALUES ('gg77', 'cashier'), ('gg99', 'master') ON CONFLICT DO NOTHING;";
         DBConnection dbConnection = new DBConnection();
