619aab658cf1858b4beefc9293ec1f1b7de30b3b
DianaDeget <89907190+DianaDeget@users.noreply.github.com>
2022-07-10 09:36:56 +0200

    User Story B3 (#3)
    - ADDED database queries to get the amount of checkouts in this month
    - ADDED database queries to get the amount of items sold in this month
    - ADDED database queries to get the total amount of sales in this month
    - ADDED needed communication between client and server to transport the amounts above to the client model manager

--- Shared/src/networking/RemoteDashboardModel.java
+++ Shared/src/networking/RemoteDashboardModel.java
@@ -9,4 +9,7 @@ public interface RemoteDashboardModel extends Remote {
     WorkingHours getWorkingHours() throws RemoteException;
     void setOpeningHours(String openingTime) throws RemoteException;
     void setClosingHours(String closingTime) throws RemoteException;
+    String getCheckoutsThisMonth() throws RemoteException;
+    String getItemsThisMonth() throws RemoteException;
+    String getSalesThisMonth() throws RemoteException;
 }
