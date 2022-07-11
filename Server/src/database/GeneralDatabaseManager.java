7a588c77b9d5b55fb01a7292414bb3edbf1bb871
iustin05 <iusgrig@icloud.com>
2022-07-11 21:53:49 +0200

    SPRINT 1 REVIEW HOTFIX 1
    - FIXED default values for preferences and users tables being inserted multiple times at each server start (added field as argument for the ON CONFLICT statement)
    - REMOVED deprecated id field in the users table
    - ADDED unique property to the role field in the users table
    - ADDED unique property to the preference field in the preferences table

--- Server/src/database/GeneralDatabaseManager.java
+++ Server/src/database/GeneralDatabaseManager.java
@@ -9,12 +9,12 @@ import java.sql.SQLException;
 public class GeneralDatabaseManager {
     public void checkDB() {
         Logger.getInstance().log(LoggerType.DEBUG, "Error-checking DB..");
-        String query = "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, password VARCHAR(100), role VARCHAR(15));" +
+        String query = "CREATE TABLE IF NOT EXISTS users (password VARCHAR(100), role VARCHAR(15) UNIQUE);" +
                 "CREATE TABLE IF NOT EXISTS items (id SERIAL PRIMARY KEY, name VARCHAR(50), quantity INT, price INT, priceControl BOOLEAN);" +
-                "CREATE TABLE IF NOT EXISTS preferences (preference VARCHAR(20), value varchar(50));" +
+                "CREATE TABLE IF NOT EXISTS preferences (preference VARCHAR(20) UNIQUE, value varchar(50));" +
                 "CREATE TABLE IF NOT EXISTS checkouts (id INT, itemID INT REFERENCES items(id), quantity INT, price INT, paymentMethod VARCHAR(15), timestamp VARCHAR(50));" +
-                "INSERT INTO preferences (preference, value) values ('workingHours', '09:00 17:00') ON CONFLICT DO NOTHING;" +
-                "INSERT INTO users (password, role) VALUES ('gg77', 'cashier'), ('gg99', 'master') ON CONFLICT DO NOTHING;";
+                "INSERT INTO preferences (preference, value) values ('workingHours', '09:00 17:00') ON CONFLICT(preference) DO NOTHING;" +
+                "INSERT INTO users (password, role) VALUES ('gg77', 'cashier'), ('gg99', 'master') ON CONFLICT(role) DO NOTHING;";
         DBConnection dbConnection = new DBConnection();
         Connection connection = dbConnection.getConnection();
         try {
