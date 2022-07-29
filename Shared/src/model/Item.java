abf763ccafc8735c95bc870cbe44a2291c6d38cd
iustin05 <iusgrig@icloud.com>
2022-07-29 17:49:59 +0200

    User Stories E6 backend + D2 R2 bugfix
    - ADDED editDateTime for every item, updated with the current time each time the price is changed
    - ADDED isEditedRecently method for items, returns true if the last edit of price for the item is within 24 hours of the current time
    - FIXED price not updating in the Inventory View after editing it, requiring restart

--- Shared/src/model/Item.java
+++ Shared/src/model/Item.java
@@ -1,21 +1,29 @@
 package model;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class Item implements Serializable {
     public Long id;
     public String name;
     public double price;
     public int quantity;
+    public LocalDateTime editDateTime;
 
-    public Item(Long id, String name, double price, int quantity) {
+    public Item(Long id, String name, double price, int quantity, LocalDateTime lastEdit) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.editDateTime = lastEdit;
     }
 
-    public Item() {}
+    public Item() {
+    }
+
+    public boolean isEditedRecently() {
+        return editDateTime.plusHours(24).isAfter(LocalDateTime.now());
+    }
 
     public Long getId() {
         return id;
@@ -39,6 +47,7 @@ public class Item implements Serializable {
 
     public void setPrice(double price) {
         this.price = price;
+        setEditDateTime(LocalDateTime.now());
     }
 
     public int getQuantity() {
@@ -47,5 +56,13 @@ public class Item implements Serializable {
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
+    }
+
+    public LocalDateTime getEditDateTime() {
+        return editDateTime;
+    }
+
+    private void setEditDateTime(LocalDateTime editDateTime) {
+        this.editDateTime = editDateTime;
     }
 }
